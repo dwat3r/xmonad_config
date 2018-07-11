9280044b21840dbfc3388e4dbc11ed6659597bb4
Oliver Hechtl <hechtloliver@gmail.com>
2018-07-11 16:01:14 +0200

    24hour format

--- xmobar.hs
+++ xmobar.hs
@@ -3,9 +3,9 @@ Config { font = "xft:Terminus"
        , fgColor = "grey"
        , position = TopW L 100
        , commands = [ Run Weather "LHBP" ["-t"," <tempC>C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
-                    , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+                    , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 20
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
-                    , Run Date "%a %b %_d %l:%M" "date" 10
+                    , Run Date "%a %b %_d %H:%M" "date" 10
                     , Run StdinReader
                     ]
        , sepChar = "%"
