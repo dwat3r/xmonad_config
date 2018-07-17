3ec1e73579816b5c7f4421a721ca2101c2db424f
Oliver Hechtl <hechtloliver@gmail.com>
2018-07-17 11:33:33 +0200

    added some cool sh*t

--- xmobar.hs
+++ xmobar.hs
@@ -1,8 +1,10 @@
 Config { font = "xft:Terminus"
        , bgColor = "black"
        , fgColor = "grey"
-       , position = TopW L 100
-       , commands = [ Run Weather "LHBP" ["-t"," <tempC>C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
+       , position = TopW L 95
+       , lowerOnStart = True
+       , commands = [ Run BatteryP ["BAT0"] ["-t", "bat: <acstatus> <left>%", "-L", "10", "-H", "80", "-p", "3","--", "-O", "<fc=green>On</fc> - ", "-i", "","-L", "-15", "-H", "-5", "-l", "red", "-m", "blue", "-h", "green"] 600
+                    , Run Weather "LHBP" ["-t"," <tempC>C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 20
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Date "%a %b %_d %H:%M" "date" 10
@@ -10,5 +12,5 @@ Config { font = "xft:Terminus"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %memory%    <fc=#ee9a00>%date%</fc> | %LHBP%"
+       , template = "%StdinReader% }{ %battery% | %cpu% | %memory%    <fc=#ee9a00>%date%</fc> | %LHBP%"
        }
--- xmonad.hs
+++ xmonad.hs
@@ -1,19 +1,32 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ToggleLayouts
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-	xmproc <- spawnPipe "xmobar /home/oliver/.xmonad/xmobar.hs"
-	xmonad defaultConfig
-		{ modMask 	= mod4Mask
-        , terminal 	= "urxvt"
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-        	{ ppOutput = hPutStrLn xmproc
-        	, ppTitle = xmobarColor "black" "" . shorten 50
-        	}
-        }
+  xmproc <- spawnPipe "xmobar /home/dwat3r/.xmonad/xmobar.hs"
+  xmonad $ fullscreenSupport $ def
+    { modMask             = mod4Mask
+    , normalBorderColor   = "#7c7c7c"
+    , focusedBorderColor  = "#ffb6b0"
+    , terminal            = "urxvtc"
+    , startupHook         = setWMName "LG3D"
+    , manageHook          = manageDocks <+> manageHook def
+    , layoutHook          = avoidStruts 
+                          $ toggleLayouts (noBorders Full) 
+                          $ smartBorders 
+                          $ layoutHook def
+    , handleEventHook     = handleEventHook def <+> docksEventHook
+    , logHook             = dynamicLogWithPP xmobarPP
+      { ppOutput            = hPutStrLn xmproc
+      , ppTitle             = xmobarColor "grey" "black" . shorten 100
+      , ppCurrent           = xmobarColor "#CEFFAC" "" . wrap "<" ">"
+      , ppSep               = "   "
+      }
+    }
