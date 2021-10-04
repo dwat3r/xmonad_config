ca3a83ae354143a050387ae16f3f5987e6005ee4
Oliver Hechtl <ohechtl@hiya.com>
2021-10-04 15:11:31 +0200

    zoom

--- xmobar.hs
+++ xmobar.hs
@@ -1,12 +1,12 @@
 Config { font = "xft:xos4 Terminus"
        , bgColor = "black"
        , fgColor = "#777777"
-       , position = TopW L 98
+       , position = TopW L 96
        , lowerOnStart = True
        , hideOnStart  = False
        , persistent   = True
        , commands = [ Run Battery [ 
-                            "-t" , "bat: <acstatus> <left>%"
+                            "-t" , "<acstatus> <left>%"
                             , "--Low"      , "10"
                             , "--High"     , "80"
                             , "--low"      , "darkred"
@@ -20,10 +20,10 @@ Config { font = "xft:xos4 Terminus"
                                        -- charged status
                                        , "-i" , ""
                              ] 50
-                    , Run Cpu ["-t","cpu: <total>%", "-L","3","-H","50","--normal","darkgreen","--high","red"] 30
-                    , Run CoreTemp [ "-t" , "<core0>°C", "--Low", "70", "--High", "80", "--low", "darkgreen", "--normal", "darkorange", "--high", "darkred" ] 50
-                    , Run Memory ["-t","mem: <usedratio>%"] 10
-                    , Run Date "%a %b %_d %H:%M" "date" 10
+                    , Run Cpu ["-t","<total>%", "-L","3","-H","50","--normal","darkgreen","--high","red"] 30
+                    , Run CoreTemp [ "-t" , "<core0>C", "--Low", "70", "--High", "80", "--low", "darkgreen", "--normal", "darkorange", "--high", "darkred" ] 50
+                    , Run Memory ["-t","<usedratio>%"] 10
+                    , Run Date "%m/%d %H:%M" "date" 10
                     , Run Wireless "wlp3s0" [ "-t", "<essid> <qualityvbar>" ] 20
                     , Run MPD ["-t", "<state>: <file>"] 10
                     , Run StdinReader
--- xmonad.hs
+++ xmonad.hs
@@ -1,76 +1,107 @@
+import System.IO
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Actions.WorkspaceNames
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicProperty
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.Place
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
+import XMonad.Layout.SimplestFloat
 import XMonad.Layout.ToggleLayouts
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
-import XMonad.Actions.CycleWS
-import System.IO
+import XMonad.Util.Run (spawnPipe)
+import qualified XMonad.StackSet as W
+
 {-
-todo: 
+todo:
  -fix "xrandr: Configure crtc 1 failed" when reconnecting external work monitors after sleep
   -> watch for xorg issue and see if it fixed yet in latest
  - make a systemd trigger for the monitor plug-in/out -> make it fail (Xorg and random cable unplug) safe
- - name workspaces
  - experiment with sessions
- - install battery power saving -> configure tlp service
- - make openvpns to a systemd service
  - find out how to connect to a new network easily with netctl
- - investigate mouse cursor X strangeness
- - configure mouse buttons to work in intellij too
- - configure aws keychain
  - try st
  - also icons to xmobar
  - figure out what the heck happens with fish colors inside urxvt
 -}
 mmask = mod4Mask
 
-main = do
-  xmproc <- spawnPipe "xmobar /home/oliver/.xmonad/xmobar.hs"
-  xmonad $ fullscreenSupport $ def
-    { modMask             = mmask
-    , normalBorderColor   = "#7c7c7c"
-    , focusedBorderColor  = "#ffb6b0"
-    , terminal            = "urxvtc"
-    , startupHook         = setWMName "LG3D"
-    , manageHook          = manageDocks <+> manageHook def
-    , layoutHook          = avoidStruts 
-                          $ toggleLayouts (noBorders Full) 
-                          $ smartBorders 
-                          $ layoutHook def
-    , handleEventHook     = handleEventHook def <+> docksEventHook
-    , logHook             = workspaceNamesPP xmobarPP
-      { ppOutput            = hPutStrLn xmproc
-      , ppTitle             = xmobarColor "grey" "black" . shorten 200
-      , ppCurrent           = xmobarColor "#CEFFAC" "black" . wrap "<" ">"
-      , ppSep               = "   "
-      } >>= dynamicLogWithPP
-      
-    } `additionalKeysP` [
-      ("M-p",     spawn "yegonesh"),
-      ("M-]",     spawn "slock"),
-      ("M-<D>",   nextWS),
-      ("M-<U>",   prevWS),
-      ("M-S-<D>", shiftToNext),
-      ("M-S-<U>", shiftToPrev),
-      ("M-<R>",   nextScreen),
-      ("M-<L>",   prevScreen),
-      ("M-S-<R>", shiftNextScreen),
-      ("M-S-<L>", shiftPrevScreen),
-      ("M-z",     toggleWS),
-      ("M-C-<D>", shiftTo Next EmptyWS),
-      ("M-C-<U>", shiftTo Prev EmptyWS),
-      ("M-C-c",   spawn "/home/oliver/scripts/keylayoutchanger.sh"),
-      ("M-S-C-]", spawn "/home/oliver/scripts/monitor_switcher.sh"),
-      ("M-S-r",   renameWorkspace def),
-      -- mpd
-      ("<XF86AudioPlay>", spawn "mpc toggle"),
-      ("<XF86AudioPrev>", spawn "mpc prev"),
-      ("<XF86AudioNext>", spawn "mpc next"),
-      ("<XF86AudioLowerVolume>", spawn "mpc volume -2"),
-      ("<XF86AudioRaiseVolume>", spawn "mpc volume +2")
+manageZoomHook =
+  composeAll
+    [ (className =? zoomClassName) <&&> shouldFloat <$> title --> doFloat,
+      (className =? zoomClassName) <&&> shouldSink <$> title --> doSink
     ]
+  where
+    zoomClassName = "zoom"
+    tileTitles =
+      [ "Zoom - Free Account", -- main window,
+          "Zoom - Licensed Account", -- main window,
+          "Zoom", -- meeting window on creation,
+          "Zoom Meeting" -- meeting window shortly after creation
+      ]
+    shouldFloat title = title `notElem` tileTitles
+    shouldSink title = title `elem` tileTitles
+    doSink = (ask >>= doF . W.sink) <+> doF W.swapDown
+
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/oliver/.xmonad/xmobar.hs"
+  xmonad $
+    fullscreenSupport $
+      def
+        { modMask = mmask,
+          normalBorderColor = "#7c7c7c",
+          focusedBorderColor = "#ffb6b0",
+          terminal = "urxvtc",
+          startupHook = setWMName "LG3D",
+          manageHook =
+            manageZoomHook
+              <+> placeHook (withGaps (16, 0, 16, 0) (smart (0.5, 0.5)))
+              <+> manageDocks
+              <+> manageHook def,
+          layoutHook =
+            avoidStruts $
+              toggleLayouts (noBorders Full) $
+                smartBorders $
+                  layoutHook def ||| simplestFloat,
+          handleEventHook = dynamicTitle manageZoomHook <+> docksEventHook <+> handleEventHook def,
+          logHook =
+            workspaceNamesPP
+              xmobarPP
+                { ppOutput = hPutStrLn xmproc,
+                  ppTitle = xmobarColor "grey" "black" . shorten 200,
+                  ppCurrent = xmobarColor "#CEFFAC" "black" . wrap "<" ">",
+                  ppSep = "   "
+                }
+              >>= dynamicLogWithPP
+        }
+        `additionalKeysP` [ ("M-p", spawn "yegonesh"),
+                            ("M-]", spawn "slock"),
+                            ("M-<D>", nextWS),
+                            ("M-<U>", prevWS),
+                            ("M-S-<D>", shiftToNext),
+                            ("M-S-<U>", shiftToPrev),
+                            ("M-<R>", nextScreen),
+                            ("M-<L>", prevScreen),
+                            ("M-S-<R>", shiftNextScreen),
+                            ("M-S-<L>", shiftPrevScreen),
+                            ("M-z", toggleWS),
+                            ("M-C-<D>", shiftTo Next EmptyWS),
+                            ("M-C-<U>", shiftTo Prev EmptyWS),
+                            ("M-C-c", spawn "/home/oliver/scripts/keylayoutchanger.sh"),
+                            ("M-S-C-]", spawn "env fish -c monitor_switcher"),
+                            ("M-S-r", renameWorkspace def),
+                            -- mpd
+                            ("M-<XF86AudioPlay>", spawn "mpc toggle"),
+                            ("M-<XF86AudioPrev>", spawn "mpc prev"),
+                            ("M-<XF86AudioNext>", spawn "mpc next"),
+                            ("M-<XF86AudioLowerVolume>", spawn "mpc volume -2"),
+                            ("M-<XF86AudioRaiseVolume>", spawn "mpc volume +2"),
+                            ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 3%-"),
+                            ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 3%+")
+                            -- ("<XF86MonBrightnessDown>", spawn ""),
+                            -- ("<XF86MonBrightnessUp>",   spawn ""),
+                            -- ("M-<F10>", spawn ""),
+                            -- ("M-<F11>", spawn "")
+                          ]
