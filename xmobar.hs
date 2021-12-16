8d512c7112c1ba579f52cbe1cbddd9f995422858
dwat3r <hechtloliver@gmail.com>
2021-12-16 12:19:57 +0100

    submodulize

--- xmobar.hs
+++ xmobar.hs
@@ -1,34 +1,47 @@
-Config { font = "xft:xos4 Terminus"
-       , bgColor = "black"
-       , fgColor = "#777777"
-       , position = TopW L 96
-       , lowerOnStart = True
-       , hideOnStart  = False
-       , persistent   = True
-       , commands = [ Run Battery [ 
-                            "-t" , "<acstatus> <left>%"
-                            , "--Low"      , "10"
-                            , "--High"     , "80"
-                            , "--low"      , "darkred"
-                            , "--normal"   , "darkorange"
-                            , "--high"     , "darkgreen"
-                            , "--" -- battery specific options
-                                       -- discharging status
-                                       , "-o" , "<fc=darkred>-</fc>(<timeleft>)"
-                                       -- AC "on" status
-                                       , "-O" , "<fc=darkorange>+</fc>(<timeleft>)"
-                                       -- charged status
-                                       , "-i" , ""
-                             ] 50
-                    , Run Cpu ["-t","<total>%", "-L","3","-H","50","--normal","darkgreen","--high","red"] 30
-                    , Run CoreTemp [ "-t" , "<core0>C", "--Low", "70", "--High", "80", "--low", "darkgreen", "--normal", "darkorange", "--high", "darkred" ] 50
-                    , Run Memory ["-t","<usedratio>%"] 10
-                    , Run Date "%m/%d %H:%M" "date" 10
-                    , Run Wireless "wlp3s0" [ "-t", "<essid> <qualityvbar>" ] 20
-                    , Run MPD ["-t", "<state>: <file>"] 10
-                    , Run StdinReader
-                    ]
-       , sepChar = "%"
-       , alignSep = "}{"
-       , template = "%StdinReader% }{ %mpd% | %wlp3s0wi% | %battery% | %cpu% %coretemp% | %memory% | <fc=darkorange>%date%</fc>"
-       }
+Config
+  { font = "xft:Terminus",
+    bgColor = "black",
+    fgColor = "#777777",
+    position = TopW L 96,
+    lowerOnStart = True,
+    hideOnStart = False,
+    persistent = True,
+    commands =
+      [ Run
+          Battery
+          [ "-t",
+            "<acstatus> <left>%",
+            "--Low",
+            "10",
+            "--High",
+            "80",
+            "--low",
+            "darkred",
+            "--normal",
+            "darkorange",
+            "--high",
+            "darkgreen",
+            "--", -- battery specific options
+            -- discharging status
+            "-o",
+            "<fc=darkred>-</fc>(<timeleft>)",
+            -- AC "on" status
+            "-O",
+            "<fc=darkorange>+</fc>(<timeleft>)",
+            -- charged status
+            "-i",
+            ""
+          ]
+          50,
+        Run Cpu ["-t", "<total>%", "-L", "3", "-H", "50", "--normal", "darkgreen", "--high", "red"] 30,
+        Run CoreTemp ["-t", "<core0>C", "--Low", "70", "--High", "80", "--low", "darkgreen", "--normal", "darkorange", "--high", "darkred"] 50,
+        Run Memory ["-t", "<usedratio>%"] 10,
+        Run Date "%m/%d %H:%M" "date" 10,
+        Run Wireless "wlp3s0" ["-t", "<essid> <qualityvbar>"] 20,
+        Run MPD ["-t", "<state>: <file>"] 10,
+        Run StdinReader
+      ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %mpd% | %wlp3s0wi% | %battery% | %cpu% %coretemp% | %memory% | <fc=darkorange>%date%</fc>"
+  }
